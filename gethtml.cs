28699e0fc39bc3083c191647c2a18f34531f9376
Leon <shakespearespride@gmail.com>
2026-05-09 14:21:22 -0300

    Create gethtml.cs

---
+++ gethtml.cs
@@ -0,0 +1,32 @@
+public class Program
+{
+    public static async Task Main()
+    {
+        Console.WriteLine("What website should we gather? ");
+        string url = Console.ReadLine() ?? "";
+        Website website = new(url);
+        Console.WriteLine(await website.getHtml());
+    }
+}
+
+public class Website
+{
+    public static readonly HttpClient _client = new();
+    public string Url {get; set; }
+    public Website(string Url)
+    {
+        this.Url = Url;
+    }
+    public async Task<string> getHtml()
+    {
+        try
+        {
+            string content = await _client.GetStringAsync(Url);
+            return content;
+        }
+        catch(Exception)
+        {
+            return "Input absolute URI";
+        }
+    }
+}
